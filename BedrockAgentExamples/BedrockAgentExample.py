17b88e8d9fc4fa141e4e1cfa26149d2e261854c0
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2024-08-30 15:00:26 +0900

    Add files via upload

--- BedrockAgentExamples/BedrockAgentExample.py
+++ BedrockAgentExamples/BedrockAgentExample.py
@@ -1,34 +1,40 @@
 import uuid
-
 import boto3
-import streamlit as st
+
 
 # Agentの定義
-agent_id: str = "XXXXXXXXXX"  # Agent IDを入力
-agent_alias_id: str = "XXXXXXXXXX"  # Alias IDを入力
+agent_id: str = "3BHIQQCNYN"  # Agent IDを入力
+agent_alias_id: str = "39DGJPPCAF"  # Alias IDを入力
+
+#agent_id: str = "PICA38JHA3"  # 0817B
+#agent_alias_id: str = "U5WNZGTHJH"  # 0817B v2
+
+prompt1 = "AnyCompany社の最新情報を教えてください。"
+prompt2 = "AnyCompany社では、6か月以上勤務した場合に与えられる有給休暇は何日ですか？"
+prompt3 = "日本で一番高い山は何ですか"
+
+prompts = [prompt1,prompt2,prompt3]
+
 session_id: str = str(uuid.uuid1())
 client = boto3.client("bedrock-agent-runtime")
 
-st.title("Agents for Amazon Bedrok チャット")
 
-if prompt := st.chat_input("質問を入力してください。"):
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        # Agentの実行
-        response = client.invoke_agent(
-            inputText=prompt,
-            agentId=agent_id,
-            agentAliasId=agent_alias_id,
-            sessionId=session_id,
-            enableTrace=False,
-        )
-
-        # Agent 実行結果の取得と表示
-        event_stream = response["completion"]
-        text = "" 
-        for event in event_stream:
-            if "chunk" in event:
-                text += event["chunk"]["bytes"].decode("utf-8")
-        st.write(text)
+for prompt in prompts:
+    # Agentの実行
+    response = client.invoke_agent(
+        inputText=prompt,
+        agentId=agent_id,
+        agentAliasId=agent_alias_id,
+        sessionId=session_id,
+        enableTrace=False,
+    )
+    
+    #Agent 実行結果の取得と表示
+    event_stream = response["completion"]
+    text = "" 
+    for event in event_stream:
+        if "chunk" in event:
+            text += event["chunk"]["bytes"].decode("utf-8")
+    print("Q: " + prompt)        
+    print("A: " +text)
+    print("-" * 80)

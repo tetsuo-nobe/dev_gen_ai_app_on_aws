74776ce11e59dcb19bfb8f8496ea8de10a9ea1fa
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2024-12-07 18:00:59 +0900

    Update BedrockKnowledgebaseExample.py

--- BedrockKnowledgebaseExamples/BedrockKnowledgebaseExample.py
+++ BedrockKnowledgebaseExamples/BedrockKnowledgebaseExample.py
@@ -6,7 +6,7 @@ kb_agent = boto3.client(service_name='bedrock-agent-runtime')
 # 
 model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
 model_arn = f'arn:aws:bedrock:us-east-1::foundation-model/{model_id}'
-kb_id = "XXXXXXXXXX" # Knowkedge BaseのID
+kb_id = "XXXXXXXXXX" # Knowkedge Base のID
 prompt = "AnyCompany社では、社員が結婚するときの休暇は何日ですか？"
 #prompt = "AnyCompany社では、6か月以上勤務した場合に与えられる有給休暇は何日ですか？"
 
@@ -21,10 +21,53 @@ response = kb_agent.retrieve_and_generate(
             'knowledgeBaseId': kb_id,
             'modelArn': model_arn
         }
-    },
+    }
 )
 
 # result
 
+print('--- 回答を出力 ---')
 generated_text = response['output']['text']
 print(generated_text)
+print('')
+################################################################
+
+# retrieve_and_generate_stream
+response = kb_agent.retrieve_and_generate_stream(
+    input={
+        'text': prompt
+    },
+    retrieveAndGenerateConfiguration={
+        'type': 'KNOWLEDGE_BASE',
+        'knowledgeBaseConfiguration': {
+            'knowledgeBaseId': kb_id,
+            'modelArn': model_arn
+        }
+    }
+)
+
+# result as stream
+print('--- 回答をストリームで出力 ---')
+stream = response.get("stream")
+
+if stream:
+    for event in stream:
+        if "output" in event:
+            print(event['output']['text'], end="", flush=True)
+
+print('')
+################################################################
+
+# retrieve
+response = kb_agent.retrieve(
+    retrievalQuery={
+        'text': prompt
+    },
+    knowledgeBaseId=kb_id
+)
+
+# result 
+
+print('--- 取得したドキュメントを出力 ---')
+for doc in response['retrievalResults']:
+  print(doc)

d60234ddf8e48a2277884f1d0700c9d15afbb481
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2025-03-11 17:42:51 +0900

    Update BedrockAgentChatExample.py

--- BedrockStreamlitExamples/BedrockAgentChatExample.py
+++ BedrockStreamlitExamples/BedrockAgentChatExample.py
@@ -1,4 +1,6 @@
-# streamlit run --your_script.py --server.port 8080
+# streamlit run your_script.py --server.port 8080
+#  Bedrock Agent の Memory や LangChain の Memory モジュールを使わない前提
+
 import uuid
 
 import boto3
@@ -11,8 +13,14 @@ ASSISTANT = "assistant"
 agent_id: str = "XXXXXXXXXX"  # Agent IDを入力
 agent_alias_id: str = "XXXXXXXXXX"  # Alias IDを入力
 
-session_id: str = str(uuid.uuid1())
-client = boto3.client("bedrock-agent-runtime")
+
+# セッションステートに client が無ければ初期化
+if "client" not in st.session_state:
+    st.session_state.client = boto3.client("bedrock-agent-runtime")
+    
+# セッションステートに session_id が無ければ初期化
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
 
 # チャット履歴保存用のセッションを初期化
 if "chat_log" not in st.session_state:
@@ -32,23 +40,22 @@ if prompt := st.chat_input("質問を入力してください。"):
 
     with st.chat_message(ASSISTANT):
         # Agentの実行
-        response = client.invoke_agent(
+        response = st.session_state.client.invoke_agent(
             inputText=prompt,
             agentId=agent_id,
             agentAliasId=agent_alias_id,
-            sessionId=session_id,
+            sessionId=st.session_state.session_id,
             enableTrace=False,
         )
 
         # Agent 実行結果の取得と表示
-        #（ストリームを処理しているようなコードだが、実際はストリームのように細切れでレスポンスは返ってきていない。
-        # https://repost.aws/questions/QU_jIzfKIAQHSXyPeE4JMAJg/issue-streaming-response-from-bedrock-agent
         event_stream = response["completion"]
         assistant_msg = "" 
         for event in event_stream:
             if "chunk" in event:
                 assistant_msg += event["chunk"]["bytes"].decode("utf-8")
         st.write(assistant_msg)
+   
     
     # セッションにチャットログを追加
     st.session_state.chat_log.append({"name": USER, "msg": prompt})

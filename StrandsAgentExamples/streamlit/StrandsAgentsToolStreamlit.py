46e9f0658ad125001b8917b0fbe481881c13f2e2
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2025-08-03 18:04:52 +0900

    Add files via upload

--- StrandsAgentExamples/streamlit/StrandsAgentsToolStreamlit.py
+++ StrandsAgentExamples/streamlit/StrandsAgentsToolStreamlit.py
@@ -1,4 +1,4 @@
-# 実行 streamlit run your_script.py --server.port 8082
+# 実行 streamlit run your_script.py --server.port 8080
 # 実行時に表示される Faild to detach context のエラーは下記の Bug の可能性
 # https://github.com/google/adk-python/issues/1670
 
@@ -8,14 +8,26 @@ import boto3
 import streamlit as st
 import asyncio
 import nest_asyncio
-from strands import Agent
+from strands import Agent, tool
 from strands.models import BedrockModel
 from strands.types.content import Messages
+from strands_tools import shell
+import os
 
 nest_asyncio.apply()
 
+# shell ツールの確認のために環境変数を設定
+# プロンプト:環境変数 DEV の値を取得して表示して下さい。
+os.environ["DEV"] = "true"
+
 USER = "user"
 ASSISTANT = "assistant"
+
+# 文字カウント関数をツールとして定義
+# プロンプト: 環境変数 DEV の値を取得してその文字数をカウントして下さい。
+@tool
+def counter(word: str, letter: str):
+    return word.lower().count(letter.lower())
 
 # model ID の設定
 model_id = "amazon.nova-lite-v1:0"
@@ -26,13 +38,18 @@ system_prompts = "あなたは優秀なアシスタントです。質問に日�
 # ストリームからテキストを取得する関数
 async def streaming(stream):
   async for event in stream:
-    text = (
-    event.get("event", {})
-          .get("contentBlockDelta", {})
-          .get("delta", {})
-          .get("text", "")
-    )
-    yield text
+    if "event" in event:
+        text = (
+        event.get("event", {})
+            .get("contentBlockDelta", {})
+            .get("delta", {})
+            .get("text", "")
+        )
+        yield text
+    elif "current_tool_use" in event:
+        current_tool_use = event.get("current_tool_use", {})
+
+        yield f"\n\n```\n🔧 Using tool: {current_tool_use}\n```\n\n"
 
 # セッションステートに agent が無ければ初期化
 if "agent" not in st.session_state:
@@ -40,6 +57,7 @@ if "agent" not in st.session_state:
     agent = Agent(
       model = model_id,
       system_prompt = system_prompts,
+      tools=[shell,counter],
       callback_handler=None  # この指定がないとPrintingCallbackHandlerによりレスポンスが自動的に標準出力に表示されてしまう。
     )
     st.session_state.agent = agent
